2434749771d0f3d79fcac906a8eefac5016a4bdd
bijokmathew <bijo.nbr@hotmail.com>
2022-12-15 15:46:19 +0000

    Added code for new task

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,13 +6,15 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("category.html")
+    return render_template("task.html")
 
 
 @app.route("/categories")
 def categories():
-    categories = list(Category.query.all())    
-    return render_template("category.html", categories=categories)
+    categories_val = list(Category.query.order_by(Category.category_name).all())
+    print(categories_val) 
+    print("============================================")
+    return render_template("category.html", categories=categories_val)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
@@ -25,6 +27,35 @@ def add_category():
     return render_template("category_add.html")
 
 
-@app.route("/add_category", methods=["GET", "POST"])
-def edit_category():
-    return render_template("category_edit.html")
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("category_edit.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
+
+
+@app.route("/add_tasks", methods=["GET", "POST"])
+def add_tasks():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task = Task(
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
+            is_urgent=bool(True if request.form.get("is_urgent") else False),
+            due_date=request.form.get("due_date"),
+            category_id=request.form.get("category_id"))
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("task_add.html", categories=categories)
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  var sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, null);
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n:{done:"Select"}
+
   });
+  var select = document.querySelectorAll('select');
+  M.FormSelect.init(select);
+});
